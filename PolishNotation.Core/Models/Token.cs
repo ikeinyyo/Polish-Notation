8d6addda0a648da446d1471223b8ddb5ae7fc7be
Sergio Gallardo Sales <gallardo91_@hotmail.com>
2015-01-11 18:10:54 +0100

    Add PolishNotation static class. It make all operations with a Polist Notation expressions

---
+++ PolishNotation.Core/Models/Token.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolishNotation.Core.Models
+{
+    public class Token
+    {
+        #region Private Const
+        private const string TokenInvalidExceptionFormat = "Token '{0}' is invalid";
+        #endregion
+        public enum TokenType { Value, Operator, Expression }
+
+        public string Text { get; set; }
+        public TokenType Type { get; set; }
+
+
+        public Token(string text)
+        {
+            Text = text;
+            parseToken();
+        }
+
+        public Token(IList<Token> tokens)
+        {
+            Text = string.Empty;
+            Type = TokenType.Expression;
+
+            if (tokens.Any())
+            {
+                Text = tokens.First().Text;
+                foreach (var token in tokens.Skip(1))
+                {
+                    Text += token.Text;
+                }
+            }
+        }
+
+        #region Private Methods
+        private void parseToken()
+        {
+            switch(Text)
+            {
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    Type = TokenType.Operator;
+                    break;
+                default:
+                    try
+                    {
+                        float.Parse(Text);
+                        Type = TokenType.Value;
+                    }
+                    catch
+                    { 
+                        throw new ArgumentException(string.Format(TokenInvalidExceptionFormat, Text))
+                    }
+                    break;
+            }
+        }
+        #endregion
+    }
+}
---
+++ PolishNotation.Core/PolishNotation.cs
@@ -0,0 +1,37 @@
+﻿using PolishNotation.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolishNotation.Core
+{
+    public static class PolishNotation
+    {
+        public static float Resolve(string expression)
+        {
+            float value = 0.0f;
+            List<Token> tokens = parseExpression(expression);
+
+
+            return value;
+        }
+
+
+        #region Private Methods
+        private static List<Token> parseExpression(string expression)
+        {
+            List<Token> tokens = new List<Token>();
+            var tokensStr = expression.Split(' ');
+
+            foreach (var token in tokensStr)
+            {
+                tokens.Add(new Token(token));
+            }
+
+            return tokens;
+        }
+        #endregion
+    }
+}
