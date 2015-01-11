ecc2b6818b24e2b424296e90da411f13cf198832
Sergio Gallardo Sales <gallardo91_@hotmail.com>
2015-01-11 19:16:26 +0100

    Add Resolve Methos in Polish Notation

--- PolishNotation.Console/Program.cs
+++ PolishNotation.Console/Program.cs
@@ -13,12 +13,12 @@ namespace PolishNotation.Console
             try
             {
                 string expressionRPN = "7 4 4 * +";
-                string expressionPN = "* + 4 4 7";
+                string expressionPN = "+ * 4 4 7";
                 System.Console.WriteLine(string.Format("Reverse Polish Notation expression: {0}", expressionRPN));
                 System.Console.WriteLine(PolishNotation.Core.ReversePolishNotation.Resolve(expressionRPN));
 
                 System.Console.WriteLine(string.Format("Polish Notation expression: {0}", expressionPN));
-                System.Console.WriteLine(PolishNotation.Core.PolishNotationHelper.Resolve(expressionPN));
+                System.Console.WriteLine(PolishNotation.Core.PolishNotation.Resolve(expressionPN));
             }
             catch(Exception ex)
             {
--- PolishNotation.Core/PolishNotation.cs
+++ PolishNotation.Core/PolishNotation.cs
@@ -14,6 +14,48 @@ namespace PolishNotation.Core
             float value = 0.0f;
             List<Token> tokens = PolishNotationHelper.parseExpression(expression);
 
+            bool changes = false;
+            do
+            {
+                changes = false;
+                Token op = null; // Operator
+                Token value1 = null; // Value 1
+                Token value2 = null; // Value 2
+
+                foreach (var token in tokens)
+                {
+                    if(token.Type.Equals(Token.TokenType.Operator))
+                    {
+                        op = token;
+                        value1 = value2 = null;
+                    }
+                    else if (op != null && value1 == null && token.Type.Equals(Token.TokenType.Value))
+                    {
+                        value1 = token;
+                        value2 = null;
+                    }
+                    else if (op != null && value1 != null && value2 == null && token.Type.Equals(Token.TokenType.Value))
+                    {
+                        value2 = token;
+                        int index = tokens.IndexOf(op);
+                        tokens.Remove(op);
+                        tokens.Remove(value1);
+                        tokens.Remove(value2);
+                        float result = op.GetResult(value1.GetValue(), value2.GetValue());
+                        tokens.Insert(index, new Token(result));
+                        changes = true;
+                        break;
+                    }
+                    else
+                    {
+                        op = value1 = value2 = null;
+                    }
+                }
+
+            } while (!tokens.Count.Equals(1) && changes);
+
+            value = tokens.First().GetValue();
+
             return value;
         }
     }
