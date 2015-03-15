439b45840757ae4e6f82ab81b8d4631dd71678ae
maktub82 <gallardo91_@hotmail.com>
2015-03-15 11:22:51 +0100

    Added Infix tests for PolishNotation

--- PolishNotation.Test/InfixConverterTest.cs
+++ PolishNotation.Test/InfixConverterTest.cs
@@ -22,5 +22,19 @@ namespace PolishNotation.Test
             string result = PolishNotation.Core.ReversePolishNotation.FromInfix(expression);
             Assert.Equal(expected, result);
         }
+
+        [Theory]
+        [InlineData("3 + 4", "+ 3 4")]
+        [InlineData("5 * 4 * 3 + 2", "+ * * 5 4 3 2")]
+        [InlineData("5 + 3 * 4 + 5 + 6 / 2", "+ + + 5 * 3 4 5 / 6 2")]
+        [InlineData("2 * 10 / 5", "/ * 2 10 5")]
+        [InlineData("2 + 3 * 5 + 6 * 3 + 4 / 2 * 4 + 6", "+ + + + 2 * 3 5 * 6 3 * / 4 2 4 6")]
+        [InlineData("4 + 5 * 4 * -3", "+ 4 * * 5 4 -3")]
+
+        public void InfixToPolishNotationTest(string expression, string expected)
+        {
+            string result = PolishNotation.Core.PolishNotation.FromInfix(expression);
+            Assert.Equal(expected, result);
+        }
     }
 }
